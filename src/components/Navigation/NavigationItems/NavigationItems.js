3b045f8fd7ffc3bfcb07c27365beeefccc57b375
Bailey Pownell <bailey.pownell@gmail.com>
2019-11-12 16:44:38 -0500

    11/12

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,8 +4,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const NavigationItems = () => (
   <ul className="NavigationItems">
-    <NavigationItem link="" active>Burger Builder</NavigationItem>
-    <NavigationItem link="" active>Checkout</NavigationItem>
+    <NavigationItem link="/" >Burger Builder</NavigationItem>
+    <NavigationItem link="/orders">Orders</NavigationItem>
   </ul>
 );
 
---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Order.css';
+
+const Order = (props) => (
+    <div className="Order">
+      <p>Ingredients: </p>
+      <p>Price: </p>
+    </div>
+)
+
+export default Order;
